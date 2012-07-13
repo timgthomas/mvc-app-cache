1037855c0e391c3025101488c1ebeeb4524e4b29
Tim G. Thomas <tim@timgthomas.com>
2012-07-12 20:32:53 -0500

    tests for mixed resources

---
+++ src/MvcAppCache.Tests/MixedTests.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+
+namespace MvcAppCache.Tests
+{
+   [TestFixture]
+   public class MixedTests : TestBase
+   {
+      [Test]
+      public void Should_add_the_specified_mixed_resources_to_the_manifest()
+      {
+         var result = new AppCacheResult();
+
+         result.AddCachedResource("/index.html");
+         result.AddCachedResource("/logo.png");
+
+         result.AddNetworkResource("/style.css");
+         result.AddNetworkResource("/app.js");
+
+         result.ExecuteResult(GetMockContext());
+
+         Assert.AreEqual(result.Content, string.Format("CACHE MANIFEST{0}CACHE:{0}/index.html{0}/logo.png{0}NETWORK:{0}/style.css{0}/app.js{0}", Environment.NewLine));
+      }
+   }
+}
